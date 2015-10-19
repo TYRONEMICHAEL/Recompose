7b4b5791e8f312ddfaa3728da20cb4bc34a011ad
TYRONEMICHAEL <tyronemichael@gmail.com>
2015-10-19 20:28:52 +0200

    Complete app

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 // Comment out tests you do not want to be tested
 // import example from './test-i-dont-want-to/test.spec';
 
-// import actions from './actions/counter.spec';
-// import reducers from './reducers/counter.spec';
-// import components from './components/Counter.spec';
-import components from './containers/App.spec';
+import actions from './actions/counter.spec';
+import reducers from './reducers/counter.spec';
+import components from './components/counter.spec';
+import app from './containers/app.spec';
